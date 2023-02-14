17d758ad18bd2e19f8f083c9b9885bb120b01bc8
JesusMunoz01 <Jesus.munoz01@utrgv.edu>
2023-02-14 13:56:22 -0600

    done updated

--- index.js
+++ index.js
@@ -44,35 +44,94 @@ app.get('/items', async (req, res) =>{
     res.render('items', {list: db1});
 });
 
-app.get('/item_view/:index', async (req, res) =>{
-    let search = req.params['index'];
+app.get('/item_view/1', async (req, res) =>{
     let obj;
     const db1 = await db.all("SELECT * FROM items;");
     for (let i of db1){
-        if (i.id == search){
+        if (i.id == 1){
             obj = i.id;
             const db2 = await db.get("SELECT * FROM items WHERE id = ?;", [obj]);
             let rdb2 = await db.all("SELECT * FROM reviews WHERE itemId = ?;", [obj]);
-        
             res.render('item_view', {list: db2, rlist: rdb2});
-        
-            app.post(`/item_view/post/:index2`, async (req, res) =>{
-                    let search2 = req.params['index2'];
-                if(obj == search2){
+            app.post(`/item_view/post/1`, async (req, res) =>{
+                if(obj == 1){
                     if(req.body.Username != '' && req.body.Reviews != ''){
                         rdb2 = await db.run("INSERT INTO reviews (name, review, itemId) VALUES (?, ?, ?);", 
                         [req.body.Username, req.body.Reviews, obj]);
-                    //res.send("Submitted");
-                    rdb2 = await db.all("SELECT * FROM reviews WHERE itemId = ?;", [obj]);
-                    res.render('item_view', {list: db2, rlist: rdb2});
+                        //res.send("Submitted");
+                        rdb2 = await db.all("SELECT * FROM reviews WHERE itemId = ?;", [obj]);
+                        db2.alert = false;
+                        res.render('item_view', {list: db2, rlist: rdb2});
                     }else{
-                            db2.alert = true;
-                            res.render(`item_view`, {list: db2, rlist: rdb2});
+                        db2.alert = true;
+                        res.render(`item_view`, {list: db2, rlist: rdb2});
                     }
                 }else{res.sendStatus(404);}
                 
             });
-            continue;
+        }
+    }
+
+});
+
+app.get('/item_view/2', async (req, res) =>{
+    let obj;
+    const db1 = await db.all("SELECT * FROM items;");
+    for (let i of db1){
+        if (i.id == 2){
+            obj = i.id;
+            const db2 = await db.get("SELECT * FROM items WHERE id = ?;", [obj]);
+            let rdb2 = await db.all("SELECT * FROM reviews WHERE itemId = ?;", [obj]);
+            res.render('item_view', {list: db2, rlist: rdb2});
+            app.post(`/item_view/post/2`, async (req, res) =>{
+                if(obj == 2)
+                {
+                    if(req.body.Username != '' && req.body.Reviews != ''){
+                        rdb2 = await db.run("INSERT INTO reviews (name, review, itemId) VALUES (?, ?, ?);", 
+                        [req.body.Username, req.body.Reviews, obj]);
+                        //res.send("Submitted");
+                        rdb2 = await db.all("SELECT * FROM reviews WHERE itemId = ?;", [obj]);
+                        db2.alert = false;
+                        res.render('item_view', {list: db2, rlist: rdb2});
+                    }else{
+                        db2.alert = true;
+                        res.render(`item_view`, {list: db2, rlist: rdb2});
+                    }
+                }else{res.sendStatus(404);}
+                
+            });
+        }
+    }
+
+});
+
+app.get('/item_view/3', async (req, res) =>{
+    let obj;
+    const db1 = await db.all("SELECT * FROM items;");
+    for (let i of db1){
+        if (i.id == 3){
+            obj = i.id;
+            const db2 = await db.get("SELECT * FROM items WHERE id = ?;", [obj]);
+            let rdb2 = await db.all("SELECT * FROM reviews WHERE itemId = ?;", [obj]);
+            res.render('item_view', {list: db2, rlist: rdb2});
+            app.post(`/item_view/post/3`, async (req, res) =>{
+                if(obj == 3)
+                {
+                    if(req.body.Username != '' && req.body.Reviews != '')
+                    {
+                        rdb2 = await db.run("INSERT INTO reviews (name, review, itemId) VALUES (?, ?, ?);", 
+                        [req.body.Username, req.body.Reviews, obj]);
+                        //res.send("Submitted");
+                        rdb2 = await db.all("SELECT * FROM reviews WHERE itemId = ?;", [obj]);
+                        db2.alert = false;
+                        res.render('item_view', {list: db2, rlist: rdb2});
+                    }else{
+                        db2.alert = true;
+                        res.render(`item_view`, {list: db2, rlist: rdb2});
+                    }
+                }else{res.sendStatus(404);}
+                
+            });
         }
     }
 
